f348f5686924f99d358884a421e27998e6c41c70
blanknamedotcom <eoinkil1405@gmail.com>
2026-09-08 15:35:09 +0100

    Last push didnt work right, trying again

--- script.js
+++ script.js
@@ -1,29 +1,11 @@
 const btn = document.getElementById("btn");
-const cards = document.querySelectorAll(".card-inner");
-const exp = document.querySelectorAll(".expl");
-const list = document.getElementById("list");
+const list = document.getElementById("sizes");
 
 if(btn) { //will run on the home page, will be skipped on the other pages
-btn.addEventListener("click", 
-    function(){
+    btn.addEventListener("click", function(){
         window.location.href = "test.html";
-});
+    });
 }
-
-cards.forEach(function(card){  //runs the flip animation when clicked
-card.addEventListener("click", 
-    function(){
-        card.classList.toggle("is-flipped");
-    });
-});
-
-exp.forEach(function(explore){  //opens the scale page
-explore.addEventListener("click", 
-    function(event){
-        event.stopPropagation(); //stops the card from spinning once pressed
-        window.location.href = "size.html";
-    });
-});
 
 //Scale data for each manufacturer
 const manufacturers = {
@@ -38,14 +20,14 @@ const manufacturers = {
         scales: ["1:1", "1:6", "1:12", "1:16", "1:18", "1:20", "1:21", "1:24", "1:25", "1:28", "1:32", "1:35", "1:43", 
             "1:48", "1:50", "1:55", "1:60", "1:70", "1:72", "1:100", "1:122", "1:200", "1:250", 
             "1:300", "1:350", "1:500", "1:540", "1:400", "1:700", "1:800"] 
-        }, //note: 1:35 is figures. Soldiers, civillians, animals etc
+        }, 
     Revell: {
             scales: ["1:1", "1:6", "1:8", "1:9", "1:12", "1:13", "1:16", "1:18", "1:20", "1:24", "1:25", "1:28",
              "1:31", "1:32", "1:35", "1:39", "1:40", "1:48", "1:50", "1:54", "1:56", "1:57", "1:64", "1:65", "1:67",
              "1:70", "1:72", "1:75", "1:76", "1:78", "1:81", "1:83", "1:87", "1:90", "1:96", "1:100", "1:104", "1:108", 
              "1:110", "1:112", "1:120", "1:125", "1:139", "1:144", "1:146", "1:150", "1:172", "1:175", "1:180", "1:200", 
              "1:220", "1:225", "1:275", "1:288", "1:350", "1:400", "1:530", "1:535", "1:542", "1:570", "1:600", "1:700", 
-             "1:720", "1:1200"] //1:13 is their dinosaur range
+             "1:720", "1:1200"] 
          },
     Heller: {
         scales: ["1:8", "1:9", "1:12", "1:16", "1:18", "1:20", "1:24", "1:25", "1:30", "1:32", "1:35", "1:40", "1:43", "1:48", 
@@ -57,35 +39,55 @@ const manufacturers = {
     Italeri : {
         scales: ["1:6", "1:7", "1:9", "1:12", "1:16", "1:22", "1:24", "1:32", "1:35", "1:48", "1:56", "1:72", "1:87", "1:100",
                  "1:144", "1:200", "1:250", "1:350", "1:500", "1:540", "1:700", "1:720"]
-  
     }
-}
+};
+
+const scaleDescriptions = {
+    "1:1": "Life sized models, most are small enough to display on a shelf.",
+    "1:8": "Large scale model sets with incredible detail, not recommended for beginners."
+};
 
 //Generating scale cards
-
 if(list){
     const allScale = [...new Set(
         Object.values(manufacturers).flatMap(manufacturer => manufacturer.scales)
-    )
-    ];
-    allScales.forEach(function(scale) {
+    )];
+
+    allScale.forEach(function(scale) {
         const card = document.createElement("li");
         card.classList.add("manufacturer-card");
         card.dataset.scale = scale;
-        const descriptions = scaleDescriptions[scale] ||
-        "A range of scale models for manufacturers.";
-        card.innerHTML = 
-        '
-        <div class="card-inner">
-        <div class="card-front">
-            <h2>${scale}</h2>
-            </div>
-            <div class="card-back">
-                <h3>${scale}</h3>
-                <p>${descriptions}</p>
-                <button class="expl">View model types:</button>
+        const descriptions = scaleDescriptions[scale] || "A range of scale models for manufacturers.";
+        card.innerHTML = `
+            <div class="card-inner">
+                <div class="card-front">
+                    <h2>${scale}</h2>
+                </div>
+                <div class="card-back">
+                    <h3>${scale}</h3>
+                    <p>${descriptions}</p>
+                    <button class="expl">View model types:</button>
                 </div>
             </div>
-            ';
-    })
+        `;
+        list.appendChild(card);
+    });
+
+    //flip aimation triggered once card is flipped
+    const cards = document.querySelectorAll(".card-inner");
+    cards.forEach(function(card){  
+        card.addEventListener("click", function(){
+            card.classList.toggle("is-flipped");
+        });
+    });
+
+    const exp = document.querySelectorAll(".expl");
+    exp.forEach(function(explore){  
+        explore.addEventListener("click", function(event){
+            event.stopPropagation(); 
+            window.location.href = "size.html";
+        });
+    });
+
+
 }
