14fe9c72eb7fac846becc367e7140653e7a95e33
blanknamedotcom <eoinkil1405@gmail.com>
2026-09-08 15:25:47 +0100

    Instead of making 110 individual cards for the scales, I wrote some code in js to generate cards based on their sizes. I will then make it so that only the sizes corresponding to each manufacturer is shown on screen

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const btn = document.getElementById("btn");
 const cards = document.querySelectorAll(".card-inner");
 const exp = document.querySelectorAll(".expl");
+const list = document.getElementById("list");
 
 if(btn) { //will run on the home page, will be skipped on the other pages
 btn.addEventListener("click", 
@@ -20,10 +21,11 @@ exp.forEach(function(explore){  //opens the scale page
 explore.addEventListener("click", 
     function(event){
         event.stopPropagation(); //stops the card from spinning once pressed
-        window.location.href = "scale1.html";
+        window.location.href = "size.html";
     });
 });
 
+//Scale data for each manufacturer
 const manufacturers = {
     Airfix: {
         scales: ["1:1", "1:6", "1:8", "1:9", "1:10", "1:11", "1:12", "1:14", "1:16", "1:20", "1:21", "1:22", "1:24", "1:25", 
@@ -33,8 +35,8 @@ const manufacturers = {
             "1:4222"]
         },
     Tamiya: {
-        scales: ["1:1", "1:6", "1:12", "1:16", "1:18", "1:20", "1:21", "1:24", "1:25", "1:28", "1:32", "1:35", 
-            "1:48", "1:43", "1:48", "1:50", "1:55", "1:60", "1:70", "1:72", "1:100", "1:122", "1:200", "1:250", 
+        scales: ["1:1", "1:6", "1:12", "1:16", "1:18", "1:20", "1:21", "1:24", "1:25", "1:28", "1:32", "1:35", "1:43", 
+            "1:48", "1:50", "1:55", "1:60", "1:70", "1:72", "1:100", "1:122", "1:200", "1:250", 
             "1:300", "1:350", "1:500", "1:540", "1:400", "1:700", "1:800"] 
         }, //note: 1:35 is figures. Soldiers, civillians, animals etc
     Revell: {
@@ -53,8 +55,37 @@ const manufacturers = {
             "1:750", "1:800", "1:1200", "1:1400", "1:1600", "1:1750", "1:2000"]
     },
     Italeri : {
-        scales: ["1:6", "1:7 ", "1:9", "1:12", "1:16", "1:22", "1:24", "1:32", "1:35", "1:48", "1:56", "1:72", "1:87", "1:100",
+        scales: ["1:6", "1:7", "1:9", "1:12", "1:16", "1:22", "1:24", "1:32", "1:35", "1:48", "1:56", "1:72", "1:87", "1:100",
                  "1:144", "1:200", "1:250", "1:350", "1:500", "1:540", "1:700", "1:720"]
   
     }
+}
+
+//Generating scale cards
+
+if(list){
+    const allScale = [...new Set(
+        Object.values(manufacturers).flatMap(manufacturer => manufacturer.scales)
+    )
+    ];
+    allScales.forEach(function(scale) {
+        const card = document.createElement("li");
+        card.classList.add("manufacturer-card");
+        card.dataset.scale = scale;
+        const descriptions = scaleDescriptions[scale] ||
+        "A range of scale models for manufacturers.";
+        card.innerHTML = 
+        '
+        <div class="card-inner">
+        <div class="card-front">
+            <h2>${scale}</h2>
+            </div>
+            <div class="card-back">
+                <h3>${scale}</h3>
+                <p>${descriptions}</p>
+                <button class="expl">View model types:</button>
+                </div>
+            </div>
+            ';
+    })
 }
